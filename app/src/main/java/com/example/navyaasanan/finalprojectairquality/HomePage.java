2cd4a1e67ee6f1977781b60ed0bba55094059698
navyaasanan <navyaas2@illinois.edu>
2018-12-10 13:11:47 -0600

    Buttons working!

--- app/src/main/java/com/example/navyaasanan/finalprojectairquality/HomePage.java
+++ app/src/main/java/com/example/navyaasanan/finalprojectairquality/HomePage.java
@@ -12,6 +12,8 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Switch;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 /**
  * Home Page for our Air Quality App
@@ -53,6 +55,14 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
             }
         });
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.City, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
 
        /**
--- app/src/main/java/com/example/navyaasanan/finalprojectairquality/InfoResults.java
+++ app/src/main/java/com/example/navyaasanan/finalprojectairquality/InfoResults.java
@@ -1,13 +1,68 @@
 package com.example.navyaasanan.finalprojectairquality;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class InfoResults extends AppCompatActivity {
+
+    private String key = "4483a60facf6499da1b2df96085435a3";
+    private String URL = "https://api.breezometer.com/air-quality/v2/current-conditions?lat={latitude}&lon={longitude}&key={Your_API_Key}&features={Features_List}";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_results);
+        final float lat = 42;
+        final float log = -88;
+
+
+        class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+            protected String doInBackground(Void... urls) {
+                try {
+                    URL url = new URL("https://api.breezometer.com/air-quality/v2/current-conditions?lat={" + lat + " }&lon={" + log + "}&key={"
+                                       + key + "}&features={breezometer_aqi}");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        bufferedReader.close();
+                        return stringBuilder.toString();
+                    }
+                    finally{
+                        urlConnection.disconnect();
+                    }
+                }
+                catch(Exception e) {
+                    Log.e("ERROR", e.getMessage(), e);
+                    return null;
+                }
+            }
+        }
+
+
+
+
+        final Button goBack = findViewById(R.id.goBack);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(InfoResults.this, HomePage.class));
+            }
+        });
     }
 }
